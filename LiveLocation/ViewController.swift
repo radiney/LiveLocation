a8ad21ac1f9b213b73acc3fd3e00f7693be7454a
radiney <99511595+radiney@users.noreply.github.com>
2022-05-13 16:58:54 -0700

    Initial Commit

---
+++ LiveLocation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveLocation
+//
+//  Created by user217360 on 5/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
